066f5c719a8631492908c1ef06f01eb661d075b9
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-01-28 15:49:57 +0300

    final

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const app = Vue.createApp({
 
     data() {
         return {
-            detailsAreVisible: false,
-            friends:[{
+                friends:[{
                 id: 'Manuel',
                 name: 'Manuel peno',
                 phone: '0785674354',
@@ -18,6 +17,32 @@ const app = Vue.createApp({
             }
         ]
         }
+    }
+});
+
+app.component('friend-list', {
+
+    template: `
+    <li>
+        <h2>{{friend.name}}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'show'}}</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+        </ul>
+    </li>
+    `,
+    data() {
+        return { 
+            detailsAreVisible: false,
+            friend:
+                {
+                    id: 'Manuel',
+                    name: 'Manuel peno',
+                    phone: '0785674354',
+                    email: 'naboth@gmail.com'
+                },
+        };
     },
 
     methods: {
